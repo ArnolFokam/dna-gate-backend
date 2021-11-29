d6a3b838f979bda541805b112afc67176fa3440e
Fokam Manuel <arnolfokam23@gmail.com>
2021-11-29 20:27:33 +0200

    remove timeseries support to use free tier

--- crud/metrics/__init__.py
+++ crud/metrics/__init__.py
@@ -16,11 +16,10 @@ async def setup_metrics_collection(db):
         "metaField": "metadata",
         "granularity": "hours"
     },
+    expireAfterSeconds=15552000  # six months
     """
     for collection_name in collections:
         try:
-            await db[database].create_collection(collection_name,
-                                                 expireAfterSeconds=15552000  # six months
-                                                 )
+            await db[database].create_collection(collection_name)
         except CollectionInvalid:
             logging.info(f"{collection_name} collection is already in the database.")
